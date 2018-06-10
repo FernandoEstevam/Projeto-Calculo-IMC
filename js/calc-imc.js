493c538859842de0c71439fe6adf59979b49ad2d
Fernando Estevam <fernandoestevam23@gmail.com>
2018-06-10 17:16:25 -0300

    Filtrando erros de peso e altura

--- js/calc-imc.js
+++ js/calc-imc.js
@@ -15,19 +15,39 @@ for (var i = 0; i < pacientes.length; i++) {
     var imcTd = paciente.querySelector(".info-imc");
     var imc = calcImc(peso, altura);
 
-    if(altura <= 0 || altura > 2.5){
+    var valPeso = validaPeso(peso);
+    var valAltura = validaAltura(altura);
+    
+    if(!valAltura){
         paciente.classList.add("erro-imc");
         alturaTd.textContent = "null";
         imcTd.textContent = "null";
     }else {
         imcTd.textContent = imc;
     }
-    if(peso <= 0 || peso > 300){
+    if(!valPeso){
         paciente.classList.add("erro-imc");
         pesoTd.textContent = "null";
         imcTd.textContent = "null"
     }else{
         imcTd.textContent = imc;
+    }
+}
+
+
+function validaAltura(altura){
+    if(altura > 0 && altura < 2.60){
+        return true;
+    } else {
+        return false;
+    }
+}
+function validaPeso(peso){
+    
+    if(peso > 0 && peso < 400){
+        return true;
+    }else {
+        return false;
     }
 }
 
@@ -38,4 +58,3 @@ function calcImc(peso, altura) {
 
     return imc.toFixed(2);
 }
-
--- js/form.js
+++ js/form.js
@@ -8,10 +8,32 @@ addPaciente.addEventListener("click", function (event) {
     event.preventDefault();
 
     var paciente = infoPaciente(form);
-    console.log(criaTr(paciente));
+
+    addPacienteTabela(paciente);
+
 });
 
-function infoPaciente(form){
+function addPacienteTabela(paciente) {
+    var tabela = document.querySelector("tbody");
+    var criaPaciente = (criaTr(paciente));
+    var valAltura = validaAltura(paciente.altura);
+    var valPeso = validaPeso(paciente.peso);
+
+    if(!valAltura && !valPeso){
+        console.log("Altura e Peso errados");
+        return "Altura errada";
+    }else if(!valAltura){
+        console.log("Altura errada");
+        return "Altura errada";
+    }else if(!valPeso){
+        console.log("Peso errado");
+        return "Peso errado";
+    }else {
+        tabela.appendChild(criaPaciente);
+    }
+}
+
+function infoPaciente(form) {
     //Cria objeto paciente
     var paciente = {
         nome: form.nome.value,
@@ -22,22 +44,22 @@ function infoPaciente(form){
     }
     return paciente;
 }
-function criaTr(paciente){
+function criaTr(paciente) {
     //Cria uma tr
     var pacienteTr = document.createElement("tr");
     pacienteTr.classList.add("paciente");
-    
+
     //Adiciona as td dentro da tr
     pacienteTr.appendChild(criaTd(paciente.nome, "info-nome"));
     pacienteTr.appendChild(criaTd(paciente.altura, "info-altura"));
     pacienteTr.appendChild(criaTd(paciente.peso, "info-peso"));
     pacienteTr.appendChild(criaTd(paciente.gordura, "info-gordura"));
     pacienteTr.appendChild(criaTd(paciente.imc, "info-imc"));
-   
+
     return pacienteTr;
 }
 
-function criaTd(dado, classe){
+function criaTd(dado, classe) {
     //Cria td
     var td = document.createElement("td");
     td.textContent = dado;
